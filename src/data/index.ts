081c4db047bbb2112b698ca7ccde44c52280cc7a
Reza <reza.gholamrezazadeh@mindbridge.ai>
2025-12-23 16:12:42 -0500

    Add new vocabularies.

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,10 +1,12 @@
 import {Word} from "../models/word";
 import initialDict from "./v1/dict.json";
-import octDict from "./v1/dict-251021.json";
+import oct2025Dict from "./v1/dict-251021.json";
+import dec2025Dict from "./v1/dict-251223.json";
 
 const wordsData: Word[] = [
     ...(initialDict as unknown as Word[]).slice().reverse(), // later words are newer
-    ...(octDict as unknown as Word[]).slice().reverse(), // words from 21 OCT 2025
+    ...(oct2025Dict as unknown as Word[]).slice().reverse(), // words from 21 OCT 2025
+    ...(dec2025Dict as unknown as Word[]).slice().reverse(), // words from 23 DEC 2025
 ];
 
 export default wordsData;
